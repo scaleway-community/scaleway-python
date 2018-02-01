32656b18d413ecbd81e9060bb65f5aaf8e42a466
Maël Le Garrec <mlegarrec@online.net>
2018-02-01 13:06:13 +0000

    update makefile to env.mk

---
+++ env.mk
@@ -0,0 +1,9 @@
+IMAGE_NAME = python
+IMAGE_VERSION = latest
+IMAGE_VERSION_ALIASES =
+IMAGE_TITLE = Python
+IMAGE_DESCRIPTION = Python
+IMAGE_SOURCE_URL = https://github.com/scaleway-community/scaleway-python
+IMAGE_VENDOR_URL = https://www.python.org/
+DEFAULT_IMAGE_ARCH = x86_64
+IMAGE_BOOTSCRIPT = mainline 4.9
